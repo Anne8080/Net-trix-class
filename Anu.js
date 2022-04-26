ce87144f2ca66ee32f09b781f330b0a031287ce0
Anne <92435407+Anne8080@users.noreply.github.com>
2022-04-26 10:46:14 +0100

    Add files via upload

---
+++ Anu.js
@@ -0,0 +1,6 @@
+let myname ='Akinlawon Anuoluwapo';
+let p_num =08157852371;
+let email='ireakin10@gmail.com';
+let language='Javascript';
+let path='Frontend and UI/UX designer';
+console.log('Hi. My name is '+myname+', with contact +234'+p_num+' and '+email+' using '+language+' as the language for my first journey as a '+path+'.');
